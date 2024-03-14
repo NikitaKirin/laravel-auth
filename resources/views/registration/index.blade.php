9012ead8380879b88aff18dda66cee7ea14f65b5
Nikita Kirin <kirinnikita1406@gmail.com>
2024-03-14 23:14:47 +0500

    [DEV] Registration page

---
+++ resources/views/registration/index.blade.php
@@ -0,0 +1,46 @@
+<x-layout>
+    <div class="flex min-h-screen bg-gray-50 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                 alt="Your Company">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Регистрация</h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <x-card>
+                <x-card.body>
+                    <x-form action="#" method="POST">
+                        <x-form.item>
+                            <x-form.label for="name">Ваше имя</x-form.label>
+                            <x-form.input id="name" name="name" type="text" required placeholder="Имя" autofocus/>
+                        </x-form.item>
+                        <x-form.item>
+                            <x-form.label for="email">Ваш email</x-form.label>
+                            <x-form.input id="email" name="email" type="email" autocomplete="email" required
+                                          placeholder="examlpe@mail.com"/>
+                        </x-form.item>
+                        <x-form.item>
+                            <x-form.label for="password">Пароль</x-form.label>
+                            <x-form.input id="password" name="password" type="password" required
+                                          placeholder="********"/>
+                        </x-form.item>
+                        <x-form.item>
+                            <x-form.label for="password_confirmation">Повторите пароль</x-form.label>
+                            <x-form.input id="password_confirmation" name="password_confirmation"
+                                          type="password" required placeholder="********"/>
+                        </x-form.item>
+                        <x-form.item>
+                            <x-form.check>
+                                Пользовательское соглашение
+                            </x-form.check>
+                        </x-form.item>
+                        <x-form.item>
+                            <x-button type="submit">Зарегистрироваться</x-button>
+                        </x-form.item>
+                    </x-form>
+                </x-card.body>
+            </x-card>
+
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/registration', 'registration.index')->name('registration');
