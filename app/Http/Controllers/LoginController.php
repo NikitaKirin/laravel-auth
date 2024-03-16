cfd85fa5245ce66394c0600d0c07b0d3ac94e441
Nikita Kirin <kirinnikita1406@gmail.com>
2024-03-16 23:12:49 +0500

    [DEV] Login processing

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        if (!Auth::attempt($data)) {
+            return back()->withErrors([
+                'email' => 'Неверный email или пароль',
+            ])->onlyInput('email');
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->to(route('login'));
     }
 }
