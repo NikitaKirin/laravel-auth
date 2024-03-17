498e5c62970c38e0b54ea33a33bae56322f9d2dd
Nikita Kirin <kirinnikita1406@gmail.com>
2024-03-17 16:14:32 +0500

    [DEV] User profile page
    - added alpine.js
    https://alpinejs.dev/

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\User\SettingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/registration');
@@ -13,3 +14,7 @@ Route::post('/registration', RegistrationController::class)->name('registration.
 Route::view('/login', 'login.index')->name('login');
 
 Route::post('/login', LoginController::class)->name('login.store');
+
+Route::redirect('/user', '/user/settings')->name('user');
+
+Route::get('/user/settings', [SettingsController::class, 'index'])->name('user.settings');
